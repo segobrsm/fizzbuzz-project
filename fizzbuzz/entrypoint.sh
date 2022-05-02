d778be713fa259a665c23d666e2b82777d1aa39c
Ségolène <34601490+segobrsm@users.noreply.github.com>
2022-05-02 11:10:33 +0200

    Create entrypoint to check database creation (#8)

---
+++ fizzbuzz/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "HELLOOOO"
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+    echo $SQL_HOST
+    echo $SQL_PORT
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py
+
+exec "$@"
